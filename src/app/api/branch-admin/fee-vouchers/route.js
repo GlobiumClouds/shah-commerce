420f1ba5105cf7bbeac01c227c14b4985d58a74b
Shoaib Raza <hafizshoaibraza180@gmail.com>
2025-12-20 17:33:38 +0500

    Fee Voucher Simple PDF Work

--- src/app/api/branch-admin/fee-vouchers/route.js
+++ src/app/api/branch-admin/fee-vouchers/route.js
@@ -53,7 +53,7 @@ export const GET = withAuth(async (request, user, userDoc) => {
 
     const [vouchers, total] = await Promise.all([
       FeeVoucher.find(query)
-        .populate('studentId', 'fullName firstName lastName email studentProfile.registrationNumber studentProfile.rollNumber')
+        .populate('studentId', 'fullName firstName lastName email studentProfile.registrationNumber studentProfile.rollNumber studentProfile.section')
         .populate('templateId', 'name code category')
         .populate('classId', 'name code')
         .populate('branchId', 'name')
